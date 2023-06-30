b58b50d917fd9a2293224e2317aa9a207e6f1936
Mohammed Hafiz <www.hafiz408@gmail.com>
2023-06-30 23:53:21 +0530

    Decode patterns and basic webcam frame integration done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # importing required libraries
 import cv2
 import numpy as np
-from PIL import Image
+import time
 from env import *
 import collections
 
+# map color pixel values to valid pixel values
 def closest_value(input_value):
   arr = np.array([0, 128, 192, 255])
   i = (np.abs(arr - input_value)).argmin()
@@ -17,6 +18,25 @@ def closest_rgb(r,g,b):
 
    return (r,g,b)
 
+def find_dominant_color(frame):
+    # Convert the frame from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Reshape the frame to a flattened array of pixels
+    pixels = frame.reshape(-1, 3).astype(np.float32)
+
+    # Define the criteria for K-means clustering
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+    # Perform K-means clustering
+    _, _, centers = cv2.kmeans(pixels, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+    # Convert the center values to the uint8 data type
+    center_color = np.uint8(centers[0])
+    r, g, b = center_color
+
+    return r, g, b
+
 def partialMatch(subset):
     for i in range(len(keys)):
         j=0
@@ -28,49 +48,68 @@ def partialMatch(subset):
             print("Partial pattern ",i+1)
             return
 
-def capture_color_sequence():
+# find the valid pattern for the given 4 color sequence
+def decode_sequence(seq):
+   return valid_pattern.get(tuple(seq), 'Invalid')
+
+def capture_color_sequence(**kwargs):
     left,right=0,0
     q=collections.deque()
 
     color_seq = []
     queue = []
 
-    # taking the input from webcam
-    # vid = cv2.VideoCapture(0)
-    vid = cv2.VideoCapture('SeqOutput_TestVideo_3.avi')
+    if 'video_file' in kwargs:
+        # taking input from video file
+        vid = cv2.VideoCapture(kwargs['video_file'])
+    else:
+        # taking the input from webcam
+        vid = cv2.VideoCapture(0)
 
     milliseconds = 0
     while vid.isOpened():
-
+        
         # capturing the current frame
         valid, frame = vid.read()
         
         if not valid:
-           break
+            break
 
         # displaying the current frame
         cv2.imshow("frame", frame)
 
-        # setting values for base colors
-        b = frame[:, :, :1]
-        g = frame[:, :, 1:2]
-        r = frame[:, :, 2:]
+        if not 'video_file' in kwargs:
+            # find dominant color from frame
+            r_estimate, g_estimate, b_estimate = find_dominant_color(frame)
+        else:
+            # setting values for base colors
+            b = frame[:, :, :1]
+            g = frame[:, :, 1:2]
+            r = frame[:, :, 2:]
 
-        # computing the mean
-        b_mean = np.mean(b)
-        g_mean = np.mean(g)
-        r_mean = np.mean(r)
+            # computing the mean
+            b_estimate = np.mean(b)
+            g_estimate = np.mean(g)
+            r_estimate = np.mean(r)
 
-        # finding closest rgb
-        rgb = closest_rgb(r_mean, g_mean, b_mean)
-        color = dec_to_color.get(rgb, 404)
+        # finding closest rgb from estimate
+        rgb = closest_rgb(r_estimate, g_estimate, b_estimate)
+        color = dec_to_color.get(rgb, '404')
 
-        # print((r_mean,g_mean,b_mean), rgb, color)
+        # print((r_estimate,g_estimate,b_estimate), rgb, color)
 
         color_seq.append(color)
-        queue.append(color)
-        
-        #Code to check patterns
+
+        # avoid invalid colors
+        if color != '404':
+            queue.append(color)
+
+        # # decode sequence if queue has 4 color 
+        # if len(queue) == 4:
+        #    print(decode_sequence(queue))
+        #    queue.clear()
+
+        # Code to check patterns
         if right-left==4 and right!=0:
             tup=tuple(color_seq[left:right])
             if tup in keys:
@@ -85,13 +124,21 @@ def capture_color_sequence():
                 left=left+1
         right=right+1
 
-        # forward by milliseconds
-        milliseconds += MIN_COLOR_DURATION
-        vid.set(cv2.CAP_PROP_POS_MSEC, milliseconds)
+        if 'video_file' in kwargs:
+            # forward by milliseconds
+            milliseconds += MIN_COLOR_DURATION
+            vid.set(cv2.CAP_PROP_POS_MSEC, milliseconds)
+        else:
+            time.sleep(MIN_COLOR_DURATION*10**-3)
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    vid.release()
+    cv2.destroyAllWindows()
     return color_seq
 
-def decode_sequence(seq):
-   return valid_pattern.get(tuple(seq), 'Invalid')
-
-print(capture_color_sequence())
+print(capture_color_sequence(video_file='shortVideo.mp4'))
+# seq = capture_color_sequence()
+# print(len(seq))
